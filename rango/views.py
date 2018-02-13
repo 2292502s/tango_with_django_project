3db6efeec2afa12cf9a61974a206494b3d5f14e6
2292502s <2292502s@student.gla.ac.uk>
2018-02-13 17:09:17 +0000

    first commit

--- rango/views.py
+++ rango/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
 
 
 
 def index(request):
-   
-    return HttpResponse("Hellow World ! <br/> <a href='/rango/about/'>About</a>")
+    context = RequestContext (request)
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
